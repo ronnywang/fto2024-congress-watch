0a4f822a2f3c86d0bb49ab7801680acde7644efc
Ronny Wang <ronnywang@gmail.com>
2024-08-18 10:24:02 +0800

    add config.sample.php

---
+++ config.sample.php
@@ -0,0 +1,4 @@
+<?php
+
+putenv('WHISPERAPI_HOST='); // host name of whisper-api (https://github.com/openfunltd/whisper-api)
+putenv('WHISPERAPI_KEY='); // key of whisper-api (https://github.com/openfunltd/whisper-api)
